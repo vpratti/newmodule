91b5ca816fa3b0436c19c1afa8cb071635836bcc
Jesse Palmer <jpalmer@gannett.com>
2014-03-27 16:17:42 -0400

    layout modules panel and underscore added

--- Utilities/controllers/manageFrontsCtrl.js
+++ Utilities/controllers/manageFrontsCtrl.js
@@ -7,4 +7,14 @@ prestoApp
 		      $scope.frontsContentQueue = data;
 		    });
 		    
-    }]);
+    }])
+
+    .controller('LayoutModuleContentCtrl', function LayoutModuleContentCtrl($scope, $http) {
+       $scope.name = 'LayoutModuleContentCtrl';
+       $scope.LMC = ''
+ 
+       $http({method: 'GET', url: '/Data/LayoutModuleContent.json'}).
+   			success(function(data) {
+     			$scope.LMC = data;
+   			});
+		});
