156feed7cf74687152a598b407cb5f2ac8e2048a
Andrew Moch <amoch@gannett.com>
2014-03-27 16:41:51 -0400

    got sections looping

--- Utilities/controllers/manageFrontsCtrl.js
+++ Utilities/controllers/manageFrontsCtrl.js
@@ -1,20 +1,21 @@
 prestoApp
 	.controller('ManageFrontsCtrl', ['$scope', '$http', function($scope, $http) {
         $scope.frontsContentQueue = '';
+        $scope.LM = '';
+        $scope.LMC = '';
 
         $http({method: 'GET', url: '/Data/ContentQueueData.json'}).
 		    success(function(data) {
 		      $scope.frontsContentQueue = data;
 		    });
-		    
-    }])
 
-    .controller('LayoutModuleContentCtrl', function LayoutModuleContentCtrl($scope, $http) {
-       $scope.name = 'LayoutModuleContentCtrl';
-       $scope.LMC = ''
- 
-       $http({method: 'GET', url: '/Data/LayoutModuleContent.json'}).
+	    $http({method: 'GET', url: '/Data/LayoutModules.json'}).
+   			success(function(data) {
+     			$scope.LM = data;
+   			});
+		    
+	    $http({method: 'GET', url: '/Data/LayoutModuleContent.json'}).
    			success(function(data) {
      			$scope.LMC = data;
    			});
-		});
+    }]);
