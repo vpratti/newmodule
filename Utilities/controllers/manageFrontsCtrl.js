48a5c21927aca261dd3583e18b1bbacb9e48c77f
Venkata Pratti <vpratty@gannett.com>
2014-03-27 15:10:28 -0400

    Updated Controller

--- Utilities/controllers/manageFrontsCtrl.js
+++ Utilities/controllers/manageFrontsCtrl.js
@@ -1,4 +1,11 @@
 prestoApp
-    .controller('ManageFrontsCtrl', function ManageFrontsCtrl($scope) {
-        $scope.name = 'ManageFrontsCtrl';
-    });
+   .controller('ManageFrontsCtrl', function ManageFrontsCtrl($scope, $http) {
+       $scope.name = 'ManageFrontsCtrl';
+       $scope.frontsContentQueue = ''
+
+       $http({method: 'GET', url: '/Data/ContentQueueData.json'}).
+   success(function(data) {
+     $scope.frontsContentQueue = data;
+   });
+
+});
