46d2120fcb25757e4874e235a77fdb8550f4833e
Venkata Pratti <vpratty@gannett.com>
2014-03-27 23:06:06 -0400

    Renamed Data file, update frontqueue model to include thumbnail as a property, implemented drag from frontsqueue, implemented thumbnail image for frontqueue assets. Pending drop feature

--- Utilities/controllers/manageFrontsCtrl.js
+++ Utilities/controllers/manageFrontsCtrl.js
@@ -4,7 +4,32 @@ prestoApp
         $scope.LM = '';
         $scope.LMC = '';
 
-        $http({method: 'GET', url: '/Data/ContentQueueData.json'}).
+        $scope.draggable = { options: { effectAllowed: 'move', itemType: 'Text' } };
+        $scope.droppable = { options: { typesAllowed: ['Text', 'text/plain'], effectAllowed: 'move' } };
+
+		$scope.afterAssetDropped = function(data) {
+        };
+
+        $scope.onReorderDragEnd = function(dragkey, dropEffect) {
+        };
+
+        $scope.onDragEnded = function (dragkey, dropEffect) {
+			if (dropEffect != 'none') {
+				$scope.$apply(function() {
+					$scope.frontsContentQueue.splice(dragkey, 1);
+				});
+			}
+        };
+
+        $scope.draggable = {
+					options: {
+						effectAllowed:'move',
+						itemType: 'Text'
+					}
+		};
+
+
+        $http({method: 'GET', url: '/Data/FrontsQueue.json'}).
 		    success(function(data) {
 		      $scope.frontsContentQueue = data;
 		    });
